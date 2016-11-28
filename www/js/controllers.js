4cc1e2f884bb280ee7a6b26e1bdb3ca348217104
Valentina Da Silva <vale9003@gmail.com>
2016-11-28 12:41:51 -0300

    QR reader mobile app

---
+++ www/js/controllers.js
@@ -0,0 +1,19 @@
+angular.module('lector.controllers', ['ionic', 'ngCordova'])
+    .controller("lectorController",['$scope','$cordovaBarcodeScanner', function($scope, $cordovaBarcodeScanner) {
+  
+        $scope.leerCodigo = function() {
+            
+            
+             $cordovaBarcodeScanner.scan().then(function(imagenEscaneada) {
+                 
+                alert(imagenEscaneada.text);
+             
+             }, function(error) {
+                   
+                 alert("Ha ocurrido un error : " + error);
+             });
+            
+        }
+    
+    
+    }]);
